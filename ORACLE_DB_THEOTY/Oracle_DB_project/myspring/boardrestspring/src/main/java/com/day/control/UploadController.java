38f0a918601112152f2f33872bfbb493127ded33
leebbong <56666728+sblee1031@users.noreply.github.com>
2021-07-30 15:13:33 +0900

    답글형 게시판 다운로드 구현

--- ORACLE_DB_THEOTY/Oracle_DB_project/myspring/boardrestspring/src/main/java/com/day/control/UploadController.java
+++ ORACLE_DB_THEOTY/Oracle_DB_project/myspring/boardrestspring/src/main/java/com/day/control/UploadController.java
@@ -22,13 +22,114 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.day.dto.RepBoard;
 import com.day.dto.RepBoardFile;
+import com.day.exception.AddException;
+import com.day.service.RepBoardService;
 
 import net.coobird.thumbnailator.Thumbnailator;
 @Controller
 public class UploadController {
 	@Autowired
+	private RepBoardService service;
+	
+	
+	@Autowired
 	private ServletContext servletContext;
 	private Logger log = Logger.getLogger(UploadController.class);
+	
+	//추가한 메서드
+	@PostMapping("/upload")
+	@ResponseBody
+	public Map<String, Object> wirte(RepBoardFile repBoardFile) {
+		Map<String, Object> result = new HashMap<>();
+		//ServletUriComponentsBuilder.fromCurrentContextPath().path("upload");
+		log.info(repBoardFile.getRepBoard());
+		//log.info(repBoardFile.getProducts());
+		log.info(repBoardFile.getFoodFiles());
+		//log.info(repBoardFile.getDrinkFile());
+		try {
+			//log.error(repBoardFile.getRepBoard());
+			//System.out.println(repBoard.getBoardC());
+			service.write(repBoardFile.getRepBoard());
+			result.put("status",1);
+		} catch (AddException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("status",0);//실패
+			result.put("msg",e.getMessage());//실패
+		}
+
+		String uploadPath = servletContext.getRealPath("upload");
+		String projectPath = "E:\\JAVA_WORK\\Worksapce\\myspring\\boardrestfront\\WebContent\\upload";
+		log.info("업로드 실제경로:" + uploadPath);
+
+		//경로 생성
+		if ( ! new File(uploadPath).exists()) {
+			log.info("업로드 실제경로생성");
+			new File(uploadPath).mkdirs();
+		}
+
+		List<MultipartFile> foodFiles = repBoardFile.getFoodFiles();
+		if(foodFiles != null) {
+			for(MultipartFile food : foodFiles) {
+				if(!"".equals(food.getOriginalFilename()) && food.getSize() != 0) {
+					log.info("FOOD 파일이름:" + food.getOriginalFilename()+", 크기:" + food.getSize());
+					String[] name = food.getOriginalFilename().split("\\.");
+					//Universal Unique Identifier
+					//String fileName = UUID.randomUUID()+"_" + food.getOriginalFilename();			
+					String fileName = repBoardFile.getRepBoard().getBoardTitle();			
+					File target = new File(uploadPath, fileName+"."+name[1]);	
+					File target2 = new File(projectPath, fileName+"."+name[1]);	
+					//파일 생성
+					try {
+						FileCopyUtils.copy(food.getBytes(), target);
+						FileCopyUtils.copy(food.getBytes(), target2);
+						log.info("파일 생성");
+					} catch(Exception e) {
+						e.printStackTrace();
+
+					}
+				}
+			}
+		}
+		MultipartFile drinkFile = repBoardFile.getDrinkFile();
+		if(drinkFile != null && !"".equals(drinkFile.getOriginalFilename()) && drinkFile.getSize() != 0) {
+				log.info("FOOD 파일이름:" + drinkFile.getOriginalFilename()+", 크기:" + drinkFile.getSize());
+
+				//Universal Unique Identifier
+				String fileName = UUID.randomUUID()+"_" + drinkFile.getOriginalFilename();			
+				File target = new File(uploadPath, fileName);	
+				//파일 생성
+
+				try {
+					//이미지파일인 경우 섬네일파일을 만듦
+					String contentType = drinkFile.getContentType(); //파일형식				
+					log.info("DRINK 파일형식:" + contentType);
+					FileCopyUtils.copy(drinkFile.getBytes(), target);
+					log.info("파일 생성");
+					if(contentType.startsWith("image")) { //이미지파일인 경우
+						String thumbnailName =  "s_"+fileName; //s_파일이름
+						File thumbnailFile = new File(uploadPath,thumbnailName);
+						FileOutputStream thumbnail = new FileOutputStream(thumbnailFile);
+						InputStream drinkFileIS = drinkFile.getInputStream();
+						int width = 100;
+						int height = 100;
+
+						//섬네일파일 만들기
+						Thumbnailator.createThumbnail(drinkFileIS, thumbnail, width, height);
+
+						result.put("drinkFileName", thumbnailName);
+						return result;
+					}
+
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		return result;
+	}
+	
+	
+	
 	@PostMapping("/uploadajax")
 	public void ajax(
 			@RequestPart List<MultipartFile> foodFiles 
@@ -86,9 +187,9 @@ public class UploadController {
 		Map<String,String> result = new HashMap<>();
 		//ServletUriComponentsBuilder.fromCurrentContextPath().path("upload");
 		log.info(repBoardFile.getRepBoard());
-		log.info(repBoardFile.getProducts());
+		//log.info(repBoardFile.getProducts());
 		log.info(repBoardFile.getFoodFiles());
-		log.info(repBoardFile.getDrinkFile());
+		//log.info(repBoardFile.getDrinkFile());
 
 
 		String uploadPath = servletContext.getRealPath("upload");
